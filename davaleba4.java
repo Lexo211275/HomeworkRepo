f157d8e7385b34b75b8d981137537e2e9983b8d9
Lexo211275 <giorgashvililekso@gmail.com>
2024-10-16 18:45:53 +0400

    Add files via upload

---
+++ davaleba4.java
@@ -0,0 +1,116 @@
+public class Main {
+    public static void main(String[] args) {
+////        int a = 4;
+//        int b = 5;
+//        int c = 0;
+//        c+=a;
+//        a-=a;
+//        a+=b;
+//        b-=b;
+//        b+=c;
+//        System.out.print(a);
+//        System.out.print(b);
+        //sxva cvladis garashe
+//        int a = 4;
+//        int b = 5;
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+//        System.out.print(a);
+//        System.out.print(b);
+        //meore davaleba
+//        double a = 10.1234;
+//        int b = (int)a;
+//        System.out.print(a-b);
+        //mesame davaleba
+//        int a = 200;
+//        a/=1000;
+//        if(a>=1)
+//        {
+//            System.out.print("ar aris samnishna");
+//        }
+//        else
+//        {
+//            System.out.print("samnishnaa");
+//        }
+        //meotxe davaleba
+//        char a = 'a';
+//        char b = 'a'- 32;
+//        System.out.print(b);
+        //mexute davaleba
+//        double a = 10.5;
+//        int b = (int)a;
+//        if(a-b > b+1-a)
+//        {
+//            System.out.print(b+1);
+//        }
+//        else if(a-b < b+1-a)
+//        {
+//            System.out.print(b);
+//        }
+//        else
+//        {
+//            System.out.print(b+1);
+//        }
+        //meeqvse davaleba
+//        int a = 5;
+//        int b = 4;
+//        int k = 2;
+//        int far=a*b;
+//        int kvadr= k*k;
+//        int raod = far/kvadr;
+//                if(raod*kvadr == far)
+//                {
+//                    System.out.print(raod);
+//                }
+//                else if(raod*kvadr < far)
+//                {
+//                    System.out.print(raod+1);
+//                }
+//                else
+//                {
+//                    System.out.print(raod);
+//                }
+        //meshvide davaleba
+//        int droni = 2665;
+//        int wutni = 0;
+//        int wamni = 0;
+//        int saatni = 0;
+//        if(droni < 60)
+//        {
+//            wamni = droni;
+//        }
+//        else if(droni >= 60 && droni < 3600 && droni % 60 == 0)
+//        {
+//            wutni = droni;
+//        }
+//        else if(droni >= 3600 && droni % 3600 == 0)
+//        {
+//            saatni = droni;
+//        }
+//        else
+//        {
+//            saatni = droni / 3600;
+//            wutni = droni / 60;
+//            wamni = 2665 - (saatni * 3600 + wutni * 60);
+//        }
+//        System.out.print(saatni);
+//            System.out.print(':');
+//            System.out.print(wutni);
+//            System.out.print(':');
+//            System.out.print(wamni);
+        //merve davaleba
+//        int dro = 2665;
+//        int wuti = 0;
+//        int wami = 0;
+//        int saati = 0;
+//            saati = dro / 3600;
+//            wuti = dro / 60;
+//            wami = 2665 - (saati * 3600 + wuti * 60);
+//            System.out.print(saati);
+//            System.out.print(':');
+//            System.out.print(wuti);
+//            System.out.print(':');
+//            System.out.print(wami);
+    }
+}
