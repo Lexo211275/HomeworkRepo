ddea912518fea3674bf1159dc540c9456aa947f4
Lexo211275 <giorgashvililekso@gmail.com>
2024-10-23 19:08:17 +0400

    Add files via upload

---
+++ howmework4/src/Main.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+//        for(int a = 10; a <=100; a+=5)
+//        {
+//            System.out.println(a);
+//        }
+//        int a = 100;
+//        int b = 0;
+//        for(int i = a; i >= 1; i/=10)
+//        {
+//           b+=1;
+//        }
+//        System.out.println(b);
+//        boolean b = true;
+//        int a = 100;
+//        if(a == 1 || a == 0)
+//        {
+//            false;
+//        }
+//        else if(a == 2)
+//        {
+//            true;
+//        }
+//       for(int i = 3; i < a; i++)
+//       {
+//           if(a % i != 0)
+//           {
+//               false;
+//           }
+//       }
+//        System.out.println(b);
+        int max1 = 100;
+        int max2 = 50;
+        for(int i = max1; i > 1; i )
+    }
+}
